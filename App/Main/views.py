c524c81a9be180842d22632f9274c8baa22f951d
AliKheir <akheirali@gmail.com>
2019-10-12 16:36:39 +0300

    Make a query to the api in the views.py to return all the articles

--- App/Main/views.py
+++ App/Main/views.py
@@ -1,6 +1,22 @@
 from flask import render_template
 from app import app
+from .news_requests import get_sources,get_articles
 
 @app.route('/')
 def HomePage():
-    return render_template('sources.html')
+    """
+    Views thats renders the home page
+    """
+    general_news = get_sources('general')
+    business_news = get_sources("business")
+    sports_news = get_sources("sports")
+    return render_template('sources.html',general=general_news,business=business_news,sports=sports_news )
+
+@app.route('/News-Articles')
+def NewsArticles():
+    """
+    View that would return news articles
+     
+    """
+    health_articles = get_articles('health')
+    return render_template('articles.html')

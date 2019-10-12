8794cdfaa371ce2232f1579d2f24a9ac5c697c23
AliKheir <akheirali@gmail.com>
2019-10-12 15:02:11 +0300

    Add new method in news_requests to process first API results

--- App/news_requests.py
+++ App/news_requests.py
@@ -9,9 +9,11 @@ api_key = app.config['API_KEY']
 url = app.config['NEWS_API_BASE_URL']
 
 def get_sources(category):
+
     """
     function gets response from the api call
     """
+
     articles_url = url.format(category, api_key)
 
     with urllib.request.urlopen(articles_url) as url:
@@ -21,5 +23,21 @@ def get_sources(category):
         sources_outcome = None
         if response['sources']:
             sources_outcome_items = response['sources']
-            sources_outcome = process_results(sources_outcome_items)
+            sources_outcome = process_new_sources(sources_outcome_items)
     return sources_outcome
+
+def process_new_sources(all_sources):
+    sources_outcome = []
+    for onesource in all_sources:
+        id = onesource.get('id')
+        name = onesource.get('name')
+		description = onesource.get('description')
+		url = onesource.get('url')
+		category = onesource.get('category')
+		language = onesource.get('language')
+		country = onesource.get('country')
+
+        new_source = Sources(id, name, description, url, category, country)
+        sources_outcome.append(new_source)
+
+    return new_source
